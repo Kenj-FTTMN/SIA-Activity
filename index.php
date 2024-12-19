8a866bb90f03a5539e519c3ce049fe6c7fe27cb0
unknown <trca.mifrano.coc@phinmaed.com>
2024-12-19 08:59:57 +0800

    Navigation/Index

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <body>
+    <!-- Navigation -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -43,6 +44,7 @@
     </div>
   </div>
 </nav>
+<!-- End Of Navigation -->
     
 </body>
 </html>
