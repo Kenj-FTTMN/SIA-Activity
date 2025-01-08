b1e247ba20d66804bf8385f0a2fb1084a6856c5d
unknown <klumayog@gmail.com>
2025-01-08 22:22:29 +0800

    events

---
+++ events.php
@@ -0,0 +1,23 @@
+<p>This is our anticipated events!</p>
+<div class="photo-gallery mt-4">
+    <h2>Explore exciting events</h2>
+    <div class="row g-3">
+        <div class="col-md-4">
+            <img src="assets/images/photo4.jpg" class="img-fluid rounded shadow fixed-size">
+        </div>
+        <div class="col-md-4">
+            <img src="assets/images/photo5.png" class="img-fluid rounded shadow fixed-size">
+        </div>
+        <div class="col-md-4">
+            <img src="assets/images/photo6.jpg" class="img-fluid rounded shadow fixed-size">
+        </div>
+    </div>
+</div>
+
+<style>
+    .fixed-size {
+        width: 100%; 
+        height: 200px; 
+        object-fit: cover; /
+    }
+</style>
